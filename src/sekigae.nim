ac3040b762483291192c3e3f1b7265ca39ac7a17
jiro4989 <jiroron666@gmail.com>
2018-12-22 22:24:33 +0900

    :sparkles:JSOnファイルから席情報とID情報を取得できるようにした

--- src/sekigae.nim
+++ src/sekigae.nim
@@ -1,22 +1,44 @@
-import random, strformat
+import random, strformat, os, ospaths, json, sequtils, strutils
 import sekigae/submodule
 
-const sheets = @[
+let configPaths = [
+  ".sekigae.json",
+  getConfigDir() & &"/sekigae/config.json"
+  ]
+
+# デフォルト座席
+var sheets = @[
   @[false, false, true, true, false, false],
   @[true, true, true, true, true, true],
   @[true, true, true, true, true, true],
   @[true, true, true, true, true, true]
   ]
 
-var studentIds = @[ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" ]
+# IDリスト
+var ids = @[ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" ]
 
-if isMainModule:
+type Config = object
+  sheets: seq[seq[bool]]
+  ids: seq[string]
+
+when isMainModule:
+  # 設定ファイルがあれば読み込む
+  for configPath in configPaths:
+    if existsFile(configPath):
+      let jsonNode = configPath.readFile().parseJson()
+      let config = to(jsonNode, Config)
+      sheets = config.sheets
+      ids = config.ids
+      break
+
+  let maxIdLen = ids.mapIt(it.len).max
+
   randomize()
-  studentIds.shuffle()
+  ids.shuffle()
 
-  let newSheets = sheets.changeSheets(studentIds)
+  let newSheets = sheets.changeSheets(ids)
   for sheet in newSheets:
     var line: string
     for s in sheet:
-      line.add(&"|{s:>4}")
+      line.add("| " & align(s, maxIdLen) & " ")
     echo line, "|"

36e6ba062cd6ebcdcb2b140e08123f44156cade6
jiro4989 <jiroron666@gmail.com>
2018-12-22 23:46:01 +0900

    :bug:IDの数が足りないとエラーになるのを修正

--- src/sekigae/submodule.nim
+++ src/sekigae/submodule.nim
@@ -6,7 +6,7 @@ proc makeSheets*(sheets: seq[seq[bool]], ids: openarray[string]): auto =
   for sheet in sheets:
     var sheetLine: seq[string]
     for isAvailable in sheet:
-      if isAvailable:
+      if isAvailable and i < ids.len:
         sheetLine.add(ids[i])
         inc(i)
       else:
--- tests/tsubmodule.nim
+++ tests/tsubmodule.nim
@@ -1,8 +1,13 @@
 import unittest
 include sekigae/submodule
 
-test "makeSheets":
-  check(@[@["1"], @[""]] == @[@[true], @[false]].makeSheets(@["1"]))
-  check(@[@["1"], @["a", "A"]] == @[@[true], @[true, true]].makeSheets(@["1", "a","A"]))
-  check(@[@["1"], @["", "a"]] == @[@[true], @[false, true]].makeSheets(@["1", "a","A"]))
-  check(@[@["", ""], @["", ""]] == @[@[false, false], @[false, false]].makeSheets(@["1", "a","A"]))
+suite "makeSheets":
+  test "正常系":
+    check(@[@["1"], @[""]] == @[@[true], @[false]].makeSheets(@["1"]))
+    check(@[@["1"], @["a", "A"]] == @[@[true], @[true, true]].makeSheets(@["1", "a","A"]))
+    check(@[@["1"], @["", "a"]] == @[@[true], @[false, true]].makeSheets(@["1", "a","A"]))
+    check(@[@["", ""], @["", ""]] == @[@[false, false], @[false, false]].makeSheets(@["1", "a","A"]))
+  test "IDの数が足りない場合は空で埋まる":
+    check(@[@["1", "a"], @["A", ""]] == @[@[true, true], @[true, true]].makeSheets(@["1", "a","A"]))
+  test "IDリストに空配列を渡されたら全部空の座席が返る":
+    check(@[@["", ""], @["", ""]] == @[@[true, true], @[true, true]].makeSheets(@[]))
